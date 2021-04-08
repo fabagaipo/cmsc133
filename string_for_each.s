65114322d96d5514d2eda482ac596abeecfb17b3
F <58926653+fabagaipo@users.noreply.github.com>
2021-04-08 11:19:21 +0800

    Update string_for_each.s

--- string_for_each.s
+++ string_for_each.s
@@ -6,13 +6,13 @@ string_for_each:
 	
 	loop2:
 	sw  $a0, 4($sp)                 # Store $a0 as it will be used for argument
-    lb  $t0, 0($a0)                 # Get current character
-    beq $t0, $zero, end_for_each    # Check null character
-    jalr  $a1                       # Call callback subroutine
+    	lb  $t0, 0($a0)                 # Get current character
+    	beq $t0, $zero, end_for_each    # Check null character
+    	jalr  $a1                       # Call callback subroutine
 	lw  $a0, 4($sp)                 # Load stored $ao
-    lw  $a1, 8($sp)                 # Load stored $a1
-    addi $a0, $a0, 1             	# Increment the count for next character in string
-    j   loop2
+    	lw  $a1, 8($sp)                 # Load stored $a1
+    	addi $a0, $a0, 1             	# Increment the count for next character in string
+    	j   loop2
 
 end_for_each:
 	lw	$ra, 0($sp)					# Pop return address to caller
