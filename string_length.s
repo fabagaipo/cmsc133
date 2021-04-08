9362f0f575c7a35d881d19c2a2911a810a39fade
F <58926653+fabagaipo@users.noreply.github.com>
2021-04-08 11:20:03 +0800

    Update string_length.s

--- string_length.s
+++ string_length.s
@@ -3,7 +3,7 @@ string_length:
 
 	loop1:
 	lb $t1, 0($a0) 				# Load next character into $t1
-	beq $t1, $zero, end_for_all # Check null character
+	beq $t1, $zero, end_for_all 		# Check null character
 	addi $a0, $a0, 1 			# Increment the string pointer
 	addi $t0, $t0, 1 			# Increment the count
 	addi $v0, $t0, 0 			# Move length of string to $v0
