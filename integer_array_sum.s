2308aa30417e8a1dcf4d92204d446345d08d8de4
F <58926653+fabagaipo@users.noreply.github.com>
2021-04-08 11:19:40 +0800

    Update integer_array_sum.s

--- integer_array_sum.s
+++ integer_array_sum.s
@@ -2,13 +2,13 @@ integer_array_sum:
 
 DBG:	##### DEBUG BREAKPOINT ######
 
-    addi $v0, $zero, 0   		# Initialize Sum to zero.
+    	addi $v0, $zero, 0   			# Initialize Sum to zero.
 	add	$t0, $zero, $zero		# Initialize array index i to zero.
 	
 for_all_in_array:
 	
 	slt $t1, $t0, $a1 			# Compare, $t1 = i < sum ? 1 : 0
-	beq $t1, $0, end_for_all 	# If i is not < 10, exit the loop
+	beq $t1, $0, end_for_all 		# If i is not < 10, exit the loop
 	lw $t1, 0($a0) 				# Load current array element into t1
 	add $v0, $v0, $t1 			# Add value to sum
 	addi $t0, $t0, 1 			# Increment the count (i)
