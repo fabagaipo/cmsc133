0dc22d6a6f316a29fdf2b4bfdce3534f0d65ebfc
F <58926653+fabagaipo@users.noreply.github.com>
2021-05-04 18:06:05 +0800

    Update string_for_each.s

--- string_for_each.s
+++ string_for_each.s
@@ -2,7 +2,7 @@ string_for_each:
 
 	addi $sp, $sp, -12		# PUSH return address to caller
 	sw $ra, 0($sp)
-	sw $a1, 8($sp)                 # Store $a1
+	sw $a1, 8($sp)                 	# Store $a1
 	
 	loop2:
 	sw  $a0, 4($sp)                 # Store $a0 as it will be used for argument
